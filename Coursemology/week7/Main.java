7b1a950fc11a11c0191ae138c1a9f1334c0cbf10
aaronangxz <angxze@gmail.com>
2022-09-14 17:09:27 +0800

    Edit week 7

--- Coursemology/week7/Main.java
+++ Coursemology/week7/Main.java
@@ -8,10 +8,16 @@ public class Main {
             case LOW:
                 System.out.println(color + " indicates low priority");
                 break;
-            // ...
+            case MEDIUM:
+                System.out.println(color + " indicates medium priority");
+                break;
+            case HIGH:
+                System.out.println(color + " indicates high priority");
+                break;
         }
     }
-    //WARNING: Do not change the code below
+
+    //Do not change the code below
     public static void main(String[] args) {
         describe("Red", Priority.HIGH);
         describe("Orange", Priority.MEDIUM);
